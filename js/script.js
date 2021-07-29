6b9f7a0a9be3429e2f9ae2122e78da921af3dbcc
Evgenii <jon-95@list.ru>
2021-07-30 01:34:58 +0300

    work

---
+++ js/script.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", () => {
+  
+    const currency = document.querySelectorAll('.currency'),
+        form = document.querySelector('form'),
+        email = form.querySelector('#email'),
+        pass = form.querySelector('#password'),
+        privacy = form.querySelector('[type="checkbox"]'),
+        erremail = form.querySelector('.dataDuck__error_email'),
+        errpass = form.querySelector('.dataDuck__error_pass'),
+        errprivacy = form.querySelector('.dataDuck__error_privacy'),
+        Email = form.querySelector('.dataDuck__email'),
+        password = form.querySelector('.dataDuck__password');
+    let currencyChoose = currency[0];
+
+
+    function removeError(){
+        erremail.classList.remove('show');
+        errpass.classList.remove('show');
+        errprivacy.classList.remove('show');
+        form.querySelectorAll('input').forEach(item => {
+            defaultBorder(item);
+        });
+        
+    }
+    function defaultBorder (border){
+        border.style.border = '';
+    }
+
+    // выделяет выбранную валюту в форме и сохраняет ее в отдельную перевенную
+
+    currency.forEach((item) => {
+        
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            currency.forEach((item, i) => {
+                item.classList.remove('active');
+            });
+            event.target.classList.toggle('active');
+            currencyChoose = event.target;
+
+        });
+    });
+
+    // Удаляет действие по умолчанию у формы и проверяет введенные данные
+
+    form.addEventListener('submit', (event) =>{
+        event.preventDefault();
+        removeError();
+        if(email.value && pass.value.length >= 4 && privacy.checked){
+            // отправка формы
+        }else if(!email.value){
+            removeError();
+            erremail.classList.add('show');
+            Email.style.border = '1px solid #E15433';
+            
+        } else if (pass.value.length < 4){
+            removeError();
+            errpass.classList.add('show');
+            password.style.border = '1px solid #E15433';
+        } else {
+            removeError();
+            errprivacy.classList.add('show');
+        }
+davSDf231aVD
+    });
+
+});
